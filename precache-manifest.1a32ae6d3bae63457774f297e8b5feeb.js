9cbb9612532b35b029b1b16e10f0caf31cbeb537
AlexC <55947391+AlexCards@users.noreply.github.com>
2020-05-27 11:45:13 +0300

    Updates

---
+++ precache-manifest.1a32ae6d3bae63457774f297e8b5feeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf97157d10a211e0f457693b61f77978",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fa4c5a7e981194f82042",
+    "url": "/monsters-rolodex/static/css/main.38b7e382.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4c5a7e981194f82042",
+    "url": "/monsters-rolodex/static/js/main.63f6751f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
